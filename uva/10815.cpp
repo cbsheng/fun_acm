a6610dee6b0805005a3e17f013214fe0b88c839f
cbsheng <b.sheng.c@gmail.com>
2015-04-17 22:24:06 +0800

    add uva 10815

---
+++ uva/10815.cpp
@@ -0,0 +1,33 @@
+//https://github.com/baiyubin/pcuva-problems/tree/master/UVa%2010815%20Andys%20First%20Dictionary
+#include <string>
+#include <set>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    freopen("testdata/10815.in", "r", stdin);
+    const int nr_chr_max = 256;
+    char word[nr_chr_max + 1];
+    set<string> words;
+    while (scanf("%s", word) != EOF)
+        for (int i = 0, length = strlen(word); i < length; /* i++ */)
+        {
+            while (i < length && !isalpha(word[i]))
+                i++;
+            if (i == length)
+                break;
+            int j = 0;
+            char w[nr_chr_max + 1];
+            for ( ; i < length && isalpha(word[i]); i++, j++)
+                w[j] = static_cast<char>(tolower(word[i]));
+            w[j] = '\0';
+            words.insert(string(w));
+        }
+    for (set<string>::const_iterator i = words.begin(), e = words.end();
+            i != e; ++i)
+        printf("%s\n", (*i).c_str());
+    return 0;
+}
