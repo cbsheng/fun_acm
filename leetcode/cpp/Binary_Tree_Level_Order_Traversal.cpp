bc7e29e815f07fc7fcc33518fe9aa8a5e43b8dd3
cbsheng <b.sheng.c@gmail.com>
2015-07-26 16:09:07 +0800

    update

--- leetcode/cpp/Binary_Tree_Level_Order_Traversal.cpp
+++ leetcode/cpp/Binary_Tree_Level_Order_Traversal.cpp
@@ -9,6 +9,7 @@
  */
 class Solution {
 public:
+    //非递归版本，但是用了两个 queue，不好。
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int> > res;
         queue<TreeNode*> que;
@@ -30,5 +31,37 @@ public:
             res.push_back(v);
         }
         return res;
+    }
+};
+
+
+
+
+//非递归解法， 用了一个 queue，The Best Solution.
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int> > v;
+        if (!root) return v;
+        
+        queue<TreeNode*> que;
+        que.push(root);
+        
+        TreeNode *p = NULL;
+        int size;
+        while (!que.empty()) {
+            size = que.size();
+            vector<int> tmp;
+            for (int i = 0; i < size; ++i) {
+                p = que.front();
+                que.pop();
+                tmp.push_back(p->val);
+                if (p->left) que.push(p->left);
+                if (p->right) que.push(p->right);
+            }
+            v.push_back(tmp);
+        }
+        
+        return v;
     }
 };
