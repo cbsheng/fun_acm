ef0bceb47097e3a4f426b2d3c59303cbaa699f6a
cbsheng <b.sheng.c@gmail.com>
2015-09-01 09:29:12 +0800

    add Implement Queue by Two Stacks

---
+++ lintcode/Implement_Queue_by_Two_Stacks.cpp
@@ -0,0 +1,44 @@
+class Queue {
+public:
+    stack<int> stack1;
+    stack<int> stack2;
+
+    Queue() {
+        // do intialization if necessary
+    }
+
+    void push(int element) {
+        stack1.push(element);
+    }
+    
+    int pop() {
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.top());
+                stack1.pop();
+            }
+        }
+        if (stack2.empty()) {
+            return 0;
+        } else {
+            int res = stack2.top();
+            stack2.pop();
+            return res;
+        }
+    }
+
+    int top() {
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.top());
+                stack1.pop();
+            }
+        }
+        if (stack2.empty()) {
+            return 0;
+        } else {
+            return stack2.top();
+        }
+    }
+};
+
